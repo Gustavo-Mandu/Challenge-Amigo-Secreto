80aca21019fb6b896a8636b3bc4cf18f33f65c91
Gustavo Mandu Ferreira Matori <gmandu10@gmail.com>
2025-01-27 23:19:14 -0300

    Adicionado a função de adicionar amigo e limpar campo.

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let campo = document.querySelector('ul');
+let listaAmigos = [];
+
+function adicionarAmigo() {
+    let nomeInserido = document.querySelector('input').value;
+
+    if (nomeInserido != "") {
+    listaAmigos.push(nomeInserido);
+    campo.innerHTML += nomeInserido + "</br>";
+    limparCampo();
+
+} else alert('Por favor, insira um nome.');
+}
+
+function sortearAmigo() {
+    for (let i = 0; i < listaAmigos.length; i++) {
+        console.log(listaAmigos[i]);
+        
+    }
+}
+
+function limparCampo() {
+    nomeInserido = document.querySelector('input');
+    nomeInserido.value = '';
+}
